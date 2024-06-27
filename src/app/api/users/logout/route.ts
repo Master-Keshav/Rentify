1fdf336cee1f9b4fb497f4b061c99cde65fbe9f6
Keshav Vyas <keshav.vyas@onsite.co.in>
2024-06-27 17:30:14 +0530

    Logout Checks

--- src/app/api/users/logout/route.ts
+++ src/app/api/users/logout/route.ts
@@ -11,8 +11,11 @@ export async function GET() {
         )
         response.cookies.set("token", "",
             {
-                httpOnly: true, expires: new Date(0)
+                httpOnly: true,
+                expires: new Date(0),
+                path: "/",
             });
+        // response.headers.append('Set-Cookie', 'token=; Max-Age=0; path=/; domain=rentify-lime.vercel.app; HttpOnly');
         return response;
     } catch (error: any) {
         return NextResponse.json({ error: error.message }, { status: 500 });
